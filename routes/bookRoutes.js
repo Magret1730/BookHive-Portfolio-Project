2fbe758262b3d79bfb49f415d9fbd283ec9e2e6b
Magret <oyedeleabiodun79@gmail.com>
2024-08-10 14:14:25 -0230

    Added routes for getting all books and searching based on author, genre and title

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 import { adminCheck } from '../middleware/adminCheck.js';
 import { authenticate } from '../middleware/authenticate.js';
-import { addBook } from '../controllers/bookController.js';
+import { addBook, allBook, search } from '../controllers/bookController.js';
 
 router.post('/addBook', authenticate, adminCheck, addBook);
+router.get('/allBook', authenticate, allBook);
+router.get('/search', authenticate, search);
 
 export default router;
