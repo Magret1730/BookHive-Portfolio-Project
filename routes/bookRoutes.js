da63599b88096a880d11c4b44ccb51e824b8109f
Magret <oyedeleabiodun79@gmail.com>
2024-08-12 11:46:27 -0230

    Added route to get book based on ID

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 import { adminCheck } from '../middleware/adminCheck.js';
 import { authenticate } from '../middleware/authenticate.js';
-import { addBook, allBook, deleteBook, editBook, search } from '../controllers/bookController.js';
+import { addBook, allBook, BookById, deleteBook, editBook, search } from '../controllers/bookController.js';
 
 router.post('/addBook', authenticate, adminCheck, addBook);
 router.delete('/:id', authenticate, adminCheck, deleteBook);
 router.put('/:id', authenticate, adminCheck, editBook);
+router.get('/:id', authenticate, adminCheck, BookById);
 router.get('/', authenticate, allBook);
 router.get('/search', authenticate, search);
 
