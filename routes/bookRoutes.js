52260c38972e3f710c3b594c5fd9b4b39f192b85
Magret <oyedeleabiodun79@gmail.com>
2024-08-12 14:35:29 -0230

    Added routes to borrow and return books

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 import { adminCheck } from '../middleware/adminCheck.js';
 import { authenticate } from '../middleware/authenticate.js';
-import { addBook, allBook, BookById, deleteBook, editBook, search } from '../controllers/bookController.js';
+import { userCheck } from '../middleware/userCheck.js';
+import { addBook, allBook, BookById, borrowBook,
+    deleteBook, editBook, returnBook, search } from '../controllers/bookController.js';
 
 router.post('/addBook', authenticate, adminCheck, addBook);
 router.delete('/:id', authenticate, adminCheck, deleteBook);
@@ -11,5 +13,7 @@ router.put('/:id', authenticate, adminCheck, editBook);
 router.get('/:id', authenticate, adminCheck, BookById);
 router.get('/', authenticate, allBook);
 router.get('/search', authenticate, search);
+router.post('/borrow/:bookId', authenticate, userCheck, borrowBook);
+router.put('/borrow/:bookId', authenticate, userCheck, returnBook);
 
 export default router;
