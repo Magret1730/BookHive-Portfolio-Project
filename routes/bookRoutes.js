b756f0141ce08b2c67930b4f963b1bee642f2e44
Magret <oyedeleabiodun79@gmail.com>
2024-08-13 12:09:46 -0230

    Removed method and routes for borrowing books

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,16 +4,16 @@ const router = express.Router();
 import { adminCheck } from '../middleware/adminCheck.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { userCheck } from '../middleware/userCheck.js';
-import { addBook, allBook, BookById, borrowBook,
-    deleteBook, editBook, returnBook, search } from '../controllers/bookController.js';
+import { addBook, allBook, BookById, deleteBook, editBook, search } from '../controllers/bookController.js';
 
-router.post('/addBook', authenticate, adminCheck, addBook);
-router.delete('/:id', authenticate, adminCheck, deleteBook);
-router.put('/:id', authenticate, adminCheck, editBook);
-router.get('/:id', authenticate, adminCheck, BookById);
-router.get('/', authenticate, allBook);
-router.get('/search', authenticate, search);
-router.post('/borrow/:bookId', authenticate, userCheck, borrowBook);
-router.put('/borrow/:bookId', authenticate, userCheck, returnBook);
+// Both admin and user routes
+router.get('/', authenticate, allBook); // Route to get all books
+router.get('/search', authenticate, search); // Route to search books based on title, author or genre
+
+// Only admin routes
+router.post('/addBook', authenticate, adminCheck, addBook); // Route to add a book
+router.delete('/:id', authenticate, adminCheck, deleteBook); // (id: bookId) // Route to deletes book
+router.put('/:id', authenticate, adminCheck, editBook); // (id: bookId) // Routes to edit book details
+router.get('/:id', authenticate, adminCheck, BookById); // (id: bookId) // Routes to get books by ID
 
 export default router;
