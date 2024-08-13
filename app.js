5a3fe0f22a9a01c3df8592a08afd63dfa1fc7090
Magret <oyedeleabiodun79@gmail.com>
2024-08-13 12:08:43 -0230

    Added middleware for borrowing books

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 import db from './models/indexModel.js';
 import userRoutes from './routes/userRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
-// import redisClient from './utils/redis.js';
+import borrowRoutes from './routes/borrowRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -33,3 +33,4 @@ const environment = process.env.NODE_ENV || 'production';
 // Routes Middlewares
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/books', bookRoutes);
+app.use('/api/v1/borrow', borrowRoutes);
