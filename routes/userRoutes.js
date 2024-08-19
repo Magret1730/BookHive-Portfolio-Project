178473111958bd7ea8f73ba13c82fc6359b6fa9e
Magret <oyedeleabiodun79@gmail.com>
2024-08-19 12:21:23 -0230

    Removed logout feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import { registerUser, loginUser, logoutUser,
+import { registerUser, loginUser,
   allUsers, forgotPassword, resetPassword, 
   deleteAccount, editUserDetails} from '../controllers/userController.js';
 import { adminCheck } from '../middleware/adminCheck.js';
@@ -18,7 +18,7 @@ router.put('/edit', authenticate, userCheck, editUserDetails); // Route to edit 
 // Both admin and user routes
 router.post('/register', registerUser); // Routes to register. POST http://localhost:8000/api/v1/users/register
 router.post('/login', loginUser); // Routes to login. POST http://localhost:8000/api/v1/users/login
-router.post('/logout', authenticate, logoutUser); // Routes to logout. POST http://localhost:8000/api/v1/users/logout
+// router.post('/logout', authenticate, logoutUser); // Routes to logout. POST http://localhost:8000/api/v1/users/logout
 router.put('/forgotPassword', forgotPassword); // Routes to forgot Password. PUT http://localhost:8000/api/v1/users/forgotPassword Supply email in req.body
 router.put('/resetPassword', resetPassword); // Routes to reset Password. You will be directed from email address. PUT http://localhost:8000/api/v1/users/resetPassword Supply newPassword, resetLink in req.body
 
